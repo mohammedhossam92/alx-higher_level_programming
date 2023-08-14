45220129edbc8e8d75cd56192160b47bb84157ab
mohammed hossam <mohammedhossam5000@gmail.com>
2023-08-14 13:08:44 -0700

    test 1

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+new_list = []
+
+def replace_in_list(my_list, idx, element):
+    for (index, old_element) in enumerate(my_list):
+
+        if idx < 0:
+            return my_list
+        elif idx > len(my_list):
+            return my_list
+        else:
+            if index == idx:
+                new_list.append(element)
+            else:
+                new_list.append(old_element)
+    return new_list
