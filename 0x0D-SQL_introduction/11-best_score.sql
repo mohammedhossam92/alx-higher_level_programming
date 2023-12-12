6444aad2e59b704c4590087693802970486ed95a
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-12-12 15:36:19 -0800

    asd

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
 --select score more than or equal to 10
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
