72a6af44b5ef39f3ec4faa87d003a4ca3266df88
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-08 04:09:31 -0800

    asd

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const process = require('node:process');
-const args = process.argv;
-if (args.length === 2) {
+
+const count = process.argv.length;
+
+if (count === 2) {
   console.log('No argument');
-} else if (args.length === 3) {
+} else if (count === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
-}
