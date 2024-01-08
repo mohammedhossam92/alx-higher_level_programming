fea54840d7cba858d694b7946e8c0fbc604f5e9e
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-08 13:11:44 -0800

    a

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,19 @@
-t process = require('process');
+#!/usr/bin/node
+
+// get the arg and convert it to int
 const firstArg = process.argv[2];
 const firstArgInt = parseInt(firstArg);
 
 function factorial (n) {
-	  if (!n) {
-		      return 1;
-		    } else {
-			        if (n === 1) {
-					      return n;
-					    } else {
-						          return n * factorial(n - 1);
-						        }
-			      }
+  if (!n) {
+    return 1;
+  } else {
+    if (n === 1) {
+      return n;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
 }
 
 const fact = factorial(firstArgInt);
