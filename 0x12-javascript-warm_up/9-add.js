bdd6b15289b39fa8ab9e0d83df92a1bc82d666af
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-08 12:01:30 -0800

    nan

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// const process = require('process');
+const firstArg = process.argv[2];
+const secArg = process.argv[3];
+const firstArgInt = parseInt(firstArg);
+const secArgInt = parseInt(secArg);
+
+function add (a, b) {
+	  const ans = a + b;
+	  console.log(ans);
+}
+
+add(firstArgInt, secArgInt);
