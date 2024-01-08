22a5254d2b56c117f5c7b9abeed5f1fa748716d7
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-08 04:30:51 -0800

    3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('node:process');
+const firstArg = process.argv[2];
+
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstArg);
+}
