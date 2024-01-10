8ba9bb2e812b9c8bf0c0bdac368b0ffbc330c1f4
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-09 20:29:07 -0800

    great

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+const data = require('./101-data');
+
+// Import the dictionary from the file
+const originalDict = data.dict;
+
+// Create a new dictionary to store occurrences by user ids
+const occurrencesDict = {};
+
+// Iterate over the original dictionary
+for (const userId in originalDict) {
+  const occurrences = originalDict[userId];
+
+  // If the occurrences value is not already a key in the new dictionary, create an array for it
+  if (!(occurrences in occurrencesDict)) {
+    occurrencesDict[occurrences] = [];
+  }
+
+  // Push the current user id to the corresponding occurrences in the new dictionary
+  occurrencesDict[occurrences].push(parseInt(userId));
+}
+
+// Print the new dictionary
+console.log(occurrencesDict);
+
