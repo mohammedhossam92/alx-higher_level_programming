e02f4f4be8d97613a7ee7d8e6b6b7155b4ee467f
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-12 07:18:56 -0800

    change the name of databse to be dynamic from args

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ from sys import argv as ar
 
 if __name__ == "__main__":
     conn = db.connect(host="localhost", port=3306, user=ar[1],
-                      passwd=ar[2], db="hbtn_0e_0_usa", charset="utf8")
+                      passwd=ar[2], db=ar[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     result = cur.fetchall()
