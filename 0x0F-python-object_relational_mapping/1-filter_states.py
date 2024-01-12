d7efb25a069b9b6a06e5313a04258a86dd442ff3
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-12 07:16:51 -0800

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""list all states by name starting wih N """
+
+import MySQLdb as db
+from sys import argv as ar
+
+
+if __name__ == "__main__":
+    conn = db.connect(host="localhost", port=3306, user=ar[1],
+                      passwd=ar[2], db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cur.fetchall()
+
+    for row in result:
+        if row[1].startswith("N"):
+            print(row)
+
+    cur.close()
+    conn.close()
