6642383b33fee409cc420203740e8fe67206f9bd
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-13 01:52:32 -0800

    close db

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,6 +3,7 @@
 import MySQLdb as sql
 from sys import argv as arg
 """list all states """
+
 if __name__ == "__main__":
     conn = sql.connect(host="localhost", port=3306, user=arg[1],
                        passwd=arg[2], db=arg[3], charset="utf8")
@@ -15,3 +16,6 @@ if __name__ == "__main__":
 
     for row in rows:
         print(row)
+
+    cur.close()
+    conn.close()
