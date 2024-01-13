21a6920a7f3864b142231cda485a091f60f17e2f
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-13 01:49:26 -0800

    commenting

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,7 +2,7 @@
 
 import MySQLdb as sql
 from sys import argv as arg
-
+"""list all states """
 if __name__ == "__main__":
     conn = sql.connect(host="localhost", port=3306, user=arg[1],
                        passwd=arg[2], db=arg[3], charset="utf8")
