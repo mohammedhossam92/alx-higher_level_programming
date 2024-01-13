ed4bbea3bec7e817efacb47c706e02c2264ecbf7
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-13 02:52:47 -0800

    the parameter placed in a tuple

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import MySQLdb as sql
+from sys import argv as arg
+
+"""list all states """
+
+if __name__ == "__main__":
+    conn = sql.connect(host="localhost", port=3306, user=arg[1],
+                       passwd=arg[2], db=arg[3], charset="utf8")
+
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cur.execute(query, (arg[4],))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
