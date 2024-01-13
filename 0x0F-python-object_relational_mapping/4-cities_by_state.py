2bffb5942a05eb2fc1a253ee977253b0f1ed2f87
Mohammed Hossam <mohammedhossam5000@gmail.com>
2024-01-13 04:19:19 -0800

    a

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ from sys import argv as arg
 
 if __name__ == "__main__":
     conn = sql.connect(host="localhost", port=3306, user=arg[1], 
-                       passwd=arg[2],db=arg[3], charset="utf8")
+                       passwd=arg[2], db=arg[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
                 "JOIN states ON cities.state_id = states.id "
